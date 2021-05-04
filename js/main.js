683eef68e66b82266145e4912567356d1ec25984
ann-kar <anna.paulina.karpiuk@gmail.com>
2021-05-04 19:40:53 +0200

    scss refactoring (separate files for sections)

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    const cnt = document.querySelector(".section-container");
-    const contact = document.querySelector(".contact");
+    const header = document.querySelector(".section-header");
+    const contact = document.querySelector(".section-contact");
 
     for (let i=1; i<=200; i++) {
         let num = Math.floor(Math.random() * (10 - 1) + 1);
         let div = document.createElement("div");
         div.classList.add("square");
         div.id = "square-" + num;
-        cnt.appendChild(div);
+        header.appendChild(div);
     }
 
     for (let i=1; i<=200; i++) {
