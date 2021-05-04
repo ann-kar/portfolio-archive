a7b8a70075623b0bde5c84807c3379db0ae89f5f
ann-kar <anna.paulina.karpiuk@gmail.com>
2021-05-04 19:23:08 +0200

    project info added

--- js/main.js
+++ js/main.js
@@ -11,7 +11,6 @@ document.addEventListener("DOMContentLoaded", function() {
         cnt.appendChild(div);
     }
 
-
     for (let i=1; i<=200; i++) {
         let num = Math.floor(Math.random() * (10 - 1) + 1);
         let div = document.createElement("div");
@@ -19,5 +18,4 @@ document.addEventListener("DOMContentLoaded", function() {
         div.id = "square-" + num;
         contact.appendChild(div);
     }
-
 });
